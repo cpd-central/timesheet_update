f07dd8e4273c334d8ecff33276e8270b6dc61400
Jacob Marsnik <jmarsnik@ceg-engineers.com>
2019-11-25 07:46:30 -0600

    chnaged the path in .bat again

--- timesheet_auto_run.bat
+++ timesheet_auto_run.bat
@@ -1,2 +1,2 @@
-"Z:\CEG\Software Development\Miniconda\Scripts\activate.bat" python_data_tutorial && "Z:\CEG\Software Development\Miniconda\envs\python_data_tutorial\python.exe" "C:\cygwin64\home\jmarsnik\programming\timesheet_crawler\Timesheet Scripts\timesheetUpdateScript.py"
+"Z:\CEG\Software Development\Miniconda\Scripts\activate.bat" python_data_tutorial && "Z:\CEG\Software Development\Miniconda\envs\python_data_tutorial\python.exe" "E:\programming\timesheet_crawler\Timesheet Scripts\timesheetUpdateScript.py"
 
